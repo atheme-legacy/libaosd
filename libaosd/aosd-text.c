55ea010cf83276c85efba468415433a7d8fdf994
Eugene Paskevich <eugene@raptor.kiev.ua>
2007-12-17 17:19:51 +0200

    Prepared for inclusion of pango helpers.

---
+++ libaosd/aosd-text.c
@@ -0,0 +1,8 @@
+/* aosd -- OSD with transparency, cairo, and pango.
+ *
+ * Copyright (C) 2007 Eugene Paskevich <eugene@raptor.kiev.ua>
+ */
+
+#include "aosd-text.h"
+
+/* vim: set ts=2 sw=2 et : */
---
+++ libaosd/aosd-text.h
@@ -0,0 +1,11 @@
+/* aosd -- OSD with transparency, cairo, and pango.
+ *
+ * Copyright (C) 2007 Eugene Paskevich <eugene@raptor.kiev.ua>
+ */
+
+#ifndef __AOSD_TEXT_H__
+#define __AOSD_TEXT_H__
+
+#endif /* __AOSD_TEXT_H__ */
+
+/* vim: set ts=2 sw=2 et : */
