14da1d7ec705054cfb52b1626e5b81b481e38120
Eugene Paskevich <eugene@raptor.kiev.ua>
2008-01-26 04:42:21 +0200

    Prepared the buildsystem for aosd_cat rewriting.

---
+++ examples/aosd_cat2/aosd_cat.c
@@ -0,0 +1,11 @@
+/* aosd -- OSD with transparency, cairo, and pango.
+ *
+ * Copyright (C) 2008 Eugene Paskevich <eugene@raptor.kiev.ua>
+ */
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
+
+/* vim: set ts=2 sw=2 et : */
