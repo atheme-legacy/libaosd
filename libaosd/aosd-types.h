432574bdba8c6985138cde4d34187f5e22d8c520
Eugene Paskevich <eugene@raptor.kiev.ua>
2008-02-03 09:36:28 +0200

    Backed out revision 83bb71596305.

--- libaosd/aosd-types.h
+++ libaosd/aosd-types.h
@@ -37,19 +37,6 @@ typedef struct
   pthread_cond_t cond;
 } LockPair;
 
-#define UMODE(mode) (1 << mode)
-
-typedef enum
-{
-  UP_NONE = 0,
-  UP_HIDE,
-  UP_SHOW,
-  UP_SIZE,
-  UP_POS,
-  UP_REND,
-  UP_TIME
-} UpdateMode;
-
 struct _Aosd
 {
   Display* display;
@@ -61,8 +48,6 @@ struct _Aosd
   Colormap colormap;
   XRenderPictFormat* xrformat;
 
-  unsigned short update;
-
   AosdBackground background;
   RenderCallback renderer;
   AosdTransparency mode;
@@ -72,9 +57,6 @@ struct _Aosd
   LockPair lock_main;
   LockPair lock_update;
   int pipe[2];
-
-  struct timeval timeout_start;
-  unsigned int delta_time;
 
   Bool mouse_hide;
   Bool shown;
