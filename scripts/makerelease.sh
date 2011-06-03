177589fc8191b3618d448537acf47e24d15fbf67
JD Horelick <jdhore1@gmail.com>
2011-06-03 13:38:10 -0400

    Fix makerelease.sh

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -43,7 +43,10 @@ echo "Making release named $RELEASENAME (tip $TIP)"
 
 echo
 echo "Building root: $RELEASENAME/"
-git archive $RELEASENAME
+cd ..
+git archive --format=tar --prefix=$RELEASENAME/ HEAD | gzip >scripts/$RELEASENAME-working.tar.gz
+cd $WRKDIR
+tar -xzvf $RELEASENAME-working.tar.gz
 cd $RELEASENAME
 rm -rf .gitignore
 rm -rf scripts
@@ -58,10 +61,12 @@ fi
 cd ..
 
 echo "Building $RELEASENAME.tgz from $RELEASENAME/"
-tar zcf $RELEASENAME.tgz $RELEASENAME/
+tar zcf $RELEASENAME.tar.gz $RELEASENAME/
 
 echo "Building $RELEASENAME.tbz2 from $RELEASENAME/"
-tar jcf $RELEASENAME.tbz2 $RELEASENAME/
+tar jcf $RELEASENAME.tar.bz2 $RELEASENAME/
+
+rm $RELEASENAME-working.tar.gz
 
 PUBLISH="yes"
 
